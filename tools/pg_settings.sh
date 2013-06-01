de4b7428fcaeb76fafb67db225bd3a169d02dcf8
Chris Berkhout <chrisberkhout@gmail.com>
2013-06-01 19:02:41 +1000

    changes to PG_PASSWORD variable name.

--- tools/pg_settings.sh
+++ tools/pg_settings.sh
@@ -4,7 +4,7 @@ PG_DB=govhack
 PG_HOST=localhost
 PG_PORT=5432
 PG_USER=team
-PG_PASSWORD=team
+PGPASSWORD=team
 
-OGR_PG_CONN="dbname=$PG_DB host=$PG_HOST port=$PG_PORT user=$PG_USER password=$PG_PASSWORD"
+OGR_PG_CONN="dbname=$PG_DB host=$PG_HOST port=$PG_PORT user=$PG_USER password=$PGPASSWORD"
 
