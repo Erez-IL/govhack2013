5d8f79fea25a8e54236fa51b1d6007e65d17d6be
Chris Berkhout <chrisberkhout@gmail.com>
2013-06-01 13:51:58 +1000

    Moved misc files into directory.

--- trade.create.sql
+++
@@ -1,9 +0,0 @@
-create table trade(
-    direction   varchar(12),
-    year        year,
-    country     varchar(255),
-    comm_code   int,
-    commodity   varchar(255),
-    value       bigint  -- just in case ...
-)
-;
